37b564a9db038d73945c37125af2cbbae00af275
Daniela <daniela.arroyo27@gmail.com>
2020-03-16 09:45:31 -0500

    forms temp

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser')
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use(require('./routes/'));
 app.use(require('./routes/blogs.js'));
 app.use(require('./routes/editblogs.js'));
+
+app.get('/login', (req, res) => {
+  res.render('login')
+})
+app.get('/registration', (req, res) => {
+  res.render('registration')
+})
+app.post('/login', (req, res) => {
+  res.render('registration')
+})
+app.post('/registration', (req, res) => {
+  res.send('post registration')
+})
+
+
 
 app.listen(3000, () => {
   console.log('Listening on 3000');
